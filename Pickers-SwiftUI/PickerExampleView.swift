a6afe520ad5a8c01f5fe1c438459e843835175d5
Yuri Stanislavsky <stanislavskiy@me.com>
2021-01-04 18:13:57 +0200

    More Pickers added

--- Pickers-SwiftUI/PickerExampleView.swift
+++ Pickers-SwiftUI/PickerExampleView.swift
@@ -8,21 +8,38 @@
 import SwiftUI
 
 struct PickerExampleView: View {
-    @State private var section = 0
+    @State private var timeSection = 0
+    @State private var prioritySection = 0
+    @State private var planeModeIsOn = false
     var settingsTime = ["1 min", "5 min", "10 min", "15 min"]
+    var priority = ["!", "!!", "!!!"]
     
     var body: some View {
         NavigationView {
             Form {
-                Picker(selection: $section,
+                Picker(selection: $timeSection,
                        label: Text("Time"),
                        content: {
                         ForEach(0..<settingsTime.count) {
                             Text(settingsTime[$0])
                         }
                        })
+                
+                Toggle(isOn: $planeModeIsOn, label: {
+                    Text("Plane Mode")
+                        .foregroundColor(planeModeIsOn ? .orange : .secondary)
+                })
+                
+                Picker(selection: $prioritySection,
+                       label: Text("Priority"),
+                       content: {
+                        ForEach(0..<priority.count) {
+                            Text(priority[$0])
+                        }
+                       })
+                
             }
-            .navigationTitle("Selected: \(settingsTime[section])")
+            .navigationTitle("Selected: \(settingsTime[timeSection]) \(priority[prioritySection])")
         }
     }
 }
