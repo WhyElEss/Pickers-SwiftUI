cf628c5906055dc67cfd0d9d1234f217d4d4474a
Yuri Stanislavsky <stanislavskiy@me.com>
2021-01-04 13:15:01 +0200

    Initial Commit

---
+++ Pickers-SwiftUI/Pickers_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pickers_SwiftUIApp.swift
+//  Pickers-SwiftUI
+//
+//  Created by Юрий Станиславский on 04.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct Pickers_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
